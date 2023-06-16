ed6bf72859829e4823522ab926c0382b95664bb3
fernandobouchet <fernandobouchet@gmail.com>
2023-06-16 00:09:04 -0300

    feat: configure 'nextauth'

---
+++ app/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth/next';
+import prisma from '@/lib/prisma';
+import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  debug: process.env.NODE_ENV === 'production',
+};
+
+export default NextAuth(authOptions);
