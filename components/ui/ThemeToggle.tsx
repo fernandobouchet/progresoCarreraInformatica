fb1cb2a40fcbd614b3659e228ccdaa264eace6bc
fernandobouchet <fernandobouchet@gmail.com>
2023-06-24 17:28:52 -0300

    fix: fixed error 'Error: Hydration failed because the initial UI does not match what was rendered on the server.' on 'ThemeToggle'

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import { Button } from '@/components/ui/button';
 
 export function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <Button
