872aa417fe35fcdc212b1942c494fb4a4340e29a
fernandobouchet <fernandobouchet@gmail.com>
2023-07-31 21:26:57 -0300

    feat: add basic 'DataTable' for elements in 'admin' pages

--- app/(pages)/admin/materias/page.tsx
+++ app/(pages)/admin/materias/page.tsx
@@ -1,5 +1,16 @@
-const AdminMateriasPage = () => {
-  return <div>AdminMateriasPage</div>;
+import { DataTable } from '@/components/ui/table/DataTable';
+import { CourseColumn } from '@/components/ui/table/columns/CourseColumn';
+import prisma from '@/lib/prisma';
+
+const AdminMateriasPage = async () => {
+  const courses = await prisma.course.findMany();
+
+  return (
+    <div>
+      Materias
+      <DataTable columns={CourseColumn} data={courses} />
+    </div>
+  );
 };
 
 export default AdminMateriasPage;
--- app/(pages)/admin/usuarios/page.tsx
+++ app/(pages)/admin/usuarios/page.tsx
@@ -1,4 +1,15 @@
-const AdminUsuariosPage = () => {
-  return <div>AdminUsuariosPage</div>;
+import { UserColumn } from '@/components/ui/table/columns/UserColumn';
+import { DataTable } from '@/components/ui/table/DataTable';
+import prisma from '@/lib/prisma';
+
+const AdminUsuariosPage = async () => {
+  const users = await prisma.user.findMany();
+
+  return (
+    <div>
+      Usuarios
+      <DataTable columns={UserColumn} data={users} />
+    </div>
+  );
 };
 export default AdminUsuariosPage;
