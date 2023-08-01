6a6bb3f15da9c8acc53698e9e8ee25d9d61b36ff
fernandobouchet <fernandobouchet@gmail.com>
2023-07-31 21:26:57 -0300

    feat: add basic 'DataTable' for elements in 'admin' pages

--- app/(pages)/admin/carreras/page.tsx
+++ app/(pages)/admin/carreras/page.tsx
@@ -1,5 +1,16 @@
-const AdminCarrerasPage = () => {
-  return <div>AdminCarrerasPage</div>;
+import { DataTable } from '@/components/ui/table/DataTable';
+import { CareerColumn } from '@/components/ui/table/columns/CareerColumn';
+import prisma from '@/lib/prisma';
+
+const AdminCarrerasPage = async () => {
+  const careers = await prisma.career.findMany();
+
+  return (
+    <div>
+      Carreras
+      <DataTable columns={CareerColumn} data={careers} />
+    </div>
+  );
 };
 
 export default AdminCarrerasPage;
--- app/(pages)/admin/periodos/page.tsx
+++ app/(pages)/admin/periodos/page.tsx
@@ -1,4 +1,15 @@
-const AdminPeriodosPage = () => {
-  return <div>AdminPeriodosPage</div>;
+import { PeriodColumn } from '@/components/ui/table/columns/PeriodColumn';
+import { DataTable } from '@/components/ui/table/DataTable';
+import prisma from '@/lib/prisma';
+
+const AdminPeriodosPage = async () => {
+  const periods = await prisma.period.findMany();
+
+  return (
+    <div>
+      Periodos
+      <DataTable columns={PeriodColumn} data={periods} />
+    </div>
+  );
 };
 export default AdminPeriodosPage;
