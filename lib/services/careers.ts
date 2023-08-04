aa1b8d4bbb44628d77b0d1f65a16b5b56af32405
fernandobouchet <fernandobouchet@gmail.com>
2023-08-04 20:19:46 -0300

    fix: fixed service API Url

--- lib/services/careers.ts
+++ lib/services/careers.ts
@@ -1,5 +1,7 @@
 const getCareer = async (id: number) => {
-  const careers = await fetch(`http://localhost:3000/api/careers/${id}`);
+  const careers = await fetch(
+    `${process.env.NEXT_PUBLIC_HOME_URL}/api/careers/${id}`
+  );
   const data = await careers.json();
   return data;
 };
