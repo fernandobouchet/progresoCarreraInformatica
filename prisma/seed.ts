d3fd950553f5d21b97333a931f06f5ba377de122
fernandobouchet <fernandobouchet@gmail.com>
2023-08-06 01:37:07 -0300

    feat: update prisma 'seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,91 +1,24 @@
 import { PrismaClient, Prisma } from '@prisma/client';
+import { coursesData } from './data/courses';
+import { careersData } from './data/careers';
 
 const prisma = new PrismaClient();
 
-const careersData: Prisma.CareerCreateInput[] = [
-  {
-    name: 'Licenciatura en informática',
-  },
-  {
-    name: 'Técnicatura en informática',
-  },
-  {
-    name: 'Técnicatura en programación',
-  },
-  {
-    name: 'Técnicatura en redes y operaciones informáticas',
-  },
-  {
-    name: 'Técnicatura en inteligencia artificial',
-  },
-  {
-    name: 'Técnicatura en videojuegos',
-  },
-];
-
-const CoursesData: Prisma.CourseCreateInput[] = [
-  {
-    name: 'Matemática I',
-    area: 'CB',
-    hsWeekly: 8,
-    hsTotal: 128,
-  },
-  {
-    name: 'Introducción a la Programación',
-    area: 'AyL',
-    hsWeekly: 8,
-    hsTotal: 128,
-  },
-  {
-    name: 'Organización de Computadoras',
-    area: 'ASOyR',
-    hsWeekly: 6,
-    hsTotal: 96,
-  },
-  {
-    name: 'Nuevos Entornos y Lenguajes',
-    area: 'Otros',
-    hsWeekly: 2,
-    hsTotal: 32,
-  },
-  {
-    name: 'Estructuras de Datos',
-    area: 'TC',
-    hsWeekly: 8,
-    hsTotal: 128,
-  },
-  {
-    name: 'Programación con Objetos I',
-    area: 'AyL',
-    hsWeekly: 8,
-    hsTotal: 128,
-  },
-  {
-    name: 'Bases de Datos',
-    area: 'ISBDySI',
-    hsWeekly: 6,
-    hsTotal: 96,
-  },
-  {
-    name: 'Inglés I',
-    area: 'Otros',
-    hsWeekly: 2,
-    hsTotal: 32,
-  },
-];
+const careers: Prisma.CareerCreateInput[] = careersData;
+const courses: Prisma.CourseCreateInput[] = coursesData;
 
 async function main() {
   console.log(`Deleting previous data ...`);
   await prisma.career.deleteMany();
   await prisma.course.deleteMany();
   console.log(`Start seeding ...`);
-  for (const careerData of careersData) {
+  for (const careerData of careers) {
     const career = await prisma.career.create({
       data: careerData,
     });
     console.log(`Created career with id: ${career.id}`);
   }
-  for (const CourseData of CoursesData) {
+  for (const CourseData of courses) {
     const course = await prisma.course.create({
       data: CourseData,
     });
