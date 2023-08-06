1e6d011a2e0335c7268eb311d030d4d206f702dd
fernandobouchet <fernandobouchet@gmail.com>
2023-08-06 01:37:07 -0300

    feat: update prisma 'seed.ts

---
+++ prisma/data/careers.ts
@@ -0,0 +1,26 @@
+export const careersData = [
+  {
+    id: 1,
+    name: 'Licenciatura en informática',
+  },
+  {
+    id: 2,
+    name: 'Técnicatura en informática',
+  },
+  {
+    id: 3,
+    name: 'Técnicatura en programación',
+  },
+  {
+    id: 4,
+    name: 'Técnicatura en redes y operaciones informáticas',
+  },
+  {
+    id: 5,
+    name: 'Técnicatura en inteligencia artificial',
+  },
+  {
+    id: 6,
+    name: 'Técnicatura en videojuegos',
+  },
+];
