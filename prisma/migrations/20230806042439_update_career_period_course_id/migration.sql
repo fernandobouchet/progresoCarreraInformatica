3d9d3ab7aabc3753f59ce014dc2ffe8a3d614903
fernandobouchet <fernandobouchet@gmail.com>
2023-08-06 01:26:28 -0300

    feat(prisma): migration changes in 'id' field for 'career', 'period' and 'course'

---
+++ prisma/migrations/20230806042439_update_career_period_course_id/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `Career` MODIFY `id` INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Course` MODIFY `id` INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Period` MODIFY `id` INTEGER NOT NULL;
