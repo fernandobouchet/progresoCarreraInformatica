7de7ead3299f05182db4bf98c05a508d5ffd21aa
fernandobouchet <fernandobouchet@gmail.com>
2023-09-08 23:56:42 -0300

    feat: add 'superjson' dependencies and enable

--- context/TrpcContext.tsx
+++ context/TrpcContext.tsx
@@ -1,14 +1,11 @@
 'use client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
-import React, { useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { trpc } from '@/lib/trcp';
+import superjson from 'superjson';
 
-export default function TrpcProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function TrpcProvider({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(() =>
     trpc.createClient({
@@ -17,6 +14,7 @@ export default function TrpcProvider({
           url: 'http://localhost:3000/api/trpc',
         }),
       ],
+      transformer: superjson,
     })
   );
   return (
