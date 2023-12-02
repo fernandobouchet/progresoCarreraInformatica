aa78938c799119c8a569561b33233f7b394f65e4
fernandobouchet <fernandobouchet@gmail.com>
2023-12-02 00:25:49 -0300

    feat: update 'LoadingBar' style

--- components/ui/LoadingBar.tsx
+++ components/ui/LoadingBar.tsx
@@ -1,8 +1,8 @@
 const LoadingBar = () => {
   return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <div className="relative h-1 w-full sm:w-1/2 bg-muted mb-[25%]">
-        <div className="relative top-0 h-1 w-full rounded overflow-hidden after:absolute after:top-0 after:right-0 after:bottom-0 after:left-0 after:-translate-x-full after:bg-primary after:animate-loader" />
+    <div className="absolute top-0 left-0 h-screen w-screen flex items-center justify-center bg-background">
+      <div className="relative h-2 w-3/4 sm:w-1/2 bg-muted rounded">
+        <div className="relative top-0 h-2 w-full rounded overflow-hidden after:absolute after:top-0 after:right-0 after:bottom-0 after:left-0 after:-translate-x-full after:bg-primary after:animate-loader" />
       </div>
     </div>
   );
