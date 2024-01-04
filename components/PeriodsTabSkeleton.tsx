d642a1d83007e8dffc4b697622f95ddf3730eff7
fernandobouchet <fernandobouchet@gmail.com>
2024-01-04 00:51:21 -0300

    feat: update 'PeriodsTabSkeleton' style

--- components/PeriodsTabSkeleton.tsx
+++ components/PeriodsTabSkeleton.tsx
@@ -5,11 +5,12 @@ const PeriodsTabSkeleton = () => {
 
   return (
     <>
+      <Skeleton className="w-full py-1 h-16 flex lg:max-w-2xl mt-4 mb-11"></Skeleton>
       <div className="flex flex-col h-full w-full">
-        <Skeleton className="w-full p-0 h-14 border-none rounded-full" />
+        <Skeleton className="w-full h-14 border-none rounded-full" />
         <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-5">
           {skeletonArray.map((_, index) => (
-            <Skeleton key={index} className="h-28 w-full p-3 rounded-2xl" />
+            <Skeleton key={index} className="h-24 py-1 w-full rounded-2xl" />
           ))}
         </div>
       </div>
